178e436db452b1f2bf6e9c9e7f59d47ea60157b8
张书孟 <zhang_shu_meng@163.com>
2018-10-17 15:46:12 +0800

    [commit]: update MarginLabel

--- SwiftTool/SwiftTool/Utils/MarginLabel.swift
+++ SwiftTool/SwiftTool/Utils/MarginLabel.swift
@@ -7,21 +7,21 @@
 
 import UIKit
 
-//class MarginLabel: UILabel {
-//
-//    var contentInset: UIEdgeInsets = .zero
-//
-//    override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
-//        var rect: CGRect = super.textRect(forBounds: UIEdgeInsetsInsetRect(bounds, contentInset), limitedToNumberOfLines: numberOfLines)
-//        //根据edgeInsets，修改绘制文字的bounds
-//        rect.origin.x -= contentInset.left;
-//        rect.origin.y -= contentInset.top;
-//        rect.size.width += contentInset.left + contentInset.right;
-//        rect.size.height += contentInset.top + contentInset.bottom;
-//        return rect
-//    }
-//
-//    override func drawText(in rect: CGRect) {
-//        super.drawText(in: UIEdgeInsetsInsetRect(rect, contentInset))
-//    }
-//}
+class MarginLabel: UILabel {
+
+    var contentInset: UIEdgeInsets = .zero
+
+    override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        var rect: CGRect = super.textRect(forBounds: bounds.inset(by: contentInset), limitedToNumberOfLines: numberOfLines)
+        //根据edgeInsets，修改绘制文字的bounds
+        rect.origin.x -= contentInset.left;
+        rect.origin.y -= contentInset.top;
+        rect.size.width += contentInset.left + contentInset.right;
+        rect.size.height += contentInset.top + contentInset.bottom;
+        return rect
+    }
+
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: contentInset))
+    }
+}
